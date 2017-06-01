5774cddeafe6da739f1652234057e292e2515a19
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-06-01 13:58:46 +0200

    test: add polyfills

--- karma.conf.js
+++ karma.conf.js
@@ -35,8 +35,9 @@ module.exports = function(config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['mocha', 'chai', 'polyfill'],
 
+        polyfill: ['Promise'],
 
         // list of files / patterns to load in the browser
         files: [
