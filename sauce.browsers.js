20cbf7d1c669fa786e7cb56bb6f697aecadb115f
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-11-27 09:04:51 +0100

    test: update sauce browsers list

--- sauce.browsers.js
+++ sauce.browsers.js
@@ -1,29 +1,3 @@
-/* eslint-env node */
-
-/**
- * Copyright 2016 Chialab. All Rights Reserved.
- *
- * The MIT License (MIT)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 module.exports = {
     SL_Chrome_dev: {
         base: 'SauceLabs',
@@ -101,7 +75,7 @@ module.exports = {
         base: 'SauceLabs',
         browserName: 'safari',
         platform: 'macOS 10.12',
-        version: '10.0',
+        version: '10.1',
     },
     SL_IE_11: {
         base: 'SauceLabs',
