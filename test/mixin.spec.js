55937600c31a2ee712a3b6aea97d4f9a6e4cdea1
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-03-27 10:44:32 +0200

    test: add some regression tests for mixins

--- test/mixin.spec.js
+++ test/mixin.spec.js
@@ -22,6 +22,8 @@ const mixin = (Super) => class extends Super {
     }
 };
 
+const mixin2 = (Super) => class extends mix(Super).with(mixin) { };
+
 describe('Unit: Mixin', () => {
     describe('class creation', () => {
         const PersonClass = mix().with(mixin);
@@ -48,11 +50,17 @@ describe('Unit: Mixin', () => {
     });
 
     describe('is mixed', () => {
-        const PersonClass = mix(SuperClass).with(mixin);
+        const MiddleClass = mix(SuperClass).with(mixin);
+        const PersonClass = mix(MiddleClass).with(mixin2);
 
         it('should extend a class', () => {
             assert(mix(PersonClass).has(mixin));
-            assert(!mix(PersonClass).has(() => {}));
+            assert(mix(PersonClass).has(mixin2));
+            assert(!mix(PersonClass).has(() => { }));
+            assert(mix(MiddleClass).has(mixin));
+            assert(!mix(MiddleClass).has(mixin2));
+            assert(!mix(SuperClass).has(mixin));
+            assert(!mix(SuperClass).has(mixin2));
         });
     });
 });
