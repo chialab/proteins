ec7ef9e257cfade6d59947993cdf3e69ce62c7c3
Dario <dariosacco9@gmail.com>
2019-10-31 17:56:12 +0100

    handle custom properties for arrays and objects (wip for the objects)

--- src/merge.js
+++ src/merge.js
@@ -21,24 +21,45 @@ export default function merge(...objects) {
     }
     let first = objects.shift();
     let res = clone(first);
+    let descriptors;
     objects.forEach((obj2) => {
+        // Use descriptors to include custom properties, getters and setters.
+        descriptors = Object.getOwnPropertyDescriptors(obj2);
+
         if (isObject(res) && isObject(obj2)) {
-            Object.keys(obj2).forEach((key) => {
-                if (!options.strictMerge || first.hasOwnProperty(key)) {
-                    let entry = obj2[key];
-                    if (isObject(entry) && isObject(res[key]) && options.mergeObjects) {
-                        res[key] = merge.call(this, res[key], entry);
-                    } else if (isArray(entry) && isArray(res[key]) && options.joinArrays) {
-                        res[key] = merge.call(this, res[key], entry);
-                    }  else {
+            Object.keys(descriptors).forEach((key) => {
+                if (!options.strictMerge || !!Object.getOwnPropertyDescriptor(first, key)) {
+                    let rightDescriptor = descriptors[key];
+                    let rightVal = clone(rightDescriptor.value);
+                    let leftDescriptor = Object.getOwnPropertyDescriptor(res, key);
+                    let leftVal = leftDescriptor.value;
+
+                    if (rightVal && isObject(rightVal) && leftVal && isObject(leftVal) && options.mergeObjects) {
+                        res[key] = merge.call(this, leftVal, rightVal);
+                    } else if (isArray(rightVal) && isArray(leftVal) && options.joinArrays) {
+                        res[key] = merge.call(this, res[key], rightDescriptor);
+                    } else {
                         res[key] = clone(obj2[key]);
                     }
                 }
             });
         } else if (isArray(first) && isArray(obj2)) {
-            obj2.forEach((val) => {
-                if (first.indexOf(val) === -1) {
-                    res.push(clone(val));
+            // Skip length descriptor.
+            delete descriptors.length;
+
+            Object.keys(descriptors).forEach((key) => {
+                if (options.joinArrays &&
+                    // If property has a value
+                    descriptors[key].hasOwnProperty('value') &&
+                    // and key is a number
+                    !isNaN(key) &&
+                    // and `first` already own a property with this key
+                    !!Object.getOwnPropertyDescriptor(first, key)
+                ) {
+                    // append the value instead of overwriting.
+                    res[res.length] = clone(descriptors[key].value);
+                } else {
+                    Object.defineProperty(res, key, descriptors[key]);
                 }
             });
         } else {
--- test/merge.spec.js
+++ test/merge.spec.js
@@ -41,7 +41,7 @@ describe('Unit: Merge', () => {
         },
     };
 
-    it('should set merge two objects', () => {
+    it('should merge two objects', () => {
         let hybrid = merge(obj1, obj2);
         assert.equal(hybrid.firstName, 'Alan');
         assert.equal(hybrid.lastName, 'Rickman');
@@ -54,7 +54,7 @@ describe('Unit: Merge', () => {
         assert.equal(hybrid.awards.special.getTime(), new Date('2017/01/02').getTime());
     });
 
-    it('should set merge two objects in strict mode', () => {
+    it('should merge two objects in strict mode', () => {
         let hybrid = merge.config({ strictMerge: true })(obj1, obj2);
         assert.equal(hybrid.firstName, 'Alan');
         assert.equal(hybrid.lastName, 'Rickman');
@@ -67,7 +67,7 @@ describe('Unit: Merge', () => {
         assert.equal(hybrid.awards.special.getTime(), new Date('2017/01/02').getTime());
     });
 
-    it('should set merge two objects with array join', () => {
+    it('should merge two objects with array join', () => {
         let hybrid = merge.config({ joinArrays: true })(obj1, obj2);
         assert.equal(hybrid.firstName, 'Alan');
         assert.equal(hybrid.lastName, 'Rickman');
@@ -81,8 +81,63 @@ describe('Unit: Merge', () => {
         assert.equal(hybrid.awards.special.getTime(), new Date('2017/01/02').getTime());
     });
 
+    let array1 = [
+        'value_1_1',
+        'value_1_2',
+    ];
+    let array2 = [
+        'value_2_1',
+        'value_2_2',
+    ];
+    array2['customProp'] = 'customProp_value';
+    Object.defineProperty(array2, 'customProp2', {
+        get() {
+            return 'customProp2_value';
+        },
+        set(value) {
+            return value;
+        },
+    });
+
+    it('should merge two arrays with custom properties', () => {
+        // Values of array1 will be overwritten by those of array2 because their keys match.
+        const expected = [...array2];
+        expected['customProp'] = 'customProp_value';
+        Object.defineProperty(expected, 'customProp2', {
+            get() {
+                return 'customProp2_value';
+            },
+            set(value) {
+                return value;
+            },
+        });
+        const actual = merge(array1, array2);
+        assert.deepEqual(actual, expected);
+        assert.ownInclude(Object.getOwnPropertyNames(actual), 'customProp');
+        assert.ownInclude(Object.getOwnPropertyNames(actual), 'customProp2');
+    });
+
+    it.only('should merge two arrays with custom properties (with joinArrays: true)', () => {
+        const actual = merge.config({ joinArrays: true })(array1, array2);
+        // Values of array1 will be overwritten by those of array2 because their keys match.
+        const expected = [...array1, ...array2];
+        expected['customProp'] = 'customProp_value';
+        Object.defineProperty(expected, 'customProp2', {
+            get() {
+                return 'customProp2_value';
+            },
+            set(value) {
+                return value;
+            },
+        });
+
+        assert.deepEqual(actual, expected);
+        assert.ownInclude(Object.getOwnPropertyNames(actual), 'customProp');
+        assert.ownInclude(Object.getOwnPropertyNames(actual), 'customProp2');
+        assert.equal(actual['customProp2'], expected['customProp2']);
+    });
+
     it('should throws with incompatible types', () => {
         assert.throws(() => merge({}, 2), 'incompatible types');
     });
 });
-
