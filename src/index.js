3b4b96ab69c1bb9d8b4cc72e230d6abcae78d160
Dario <dariosacco9@gmail.com>
2019-11-05 13:03:53 +0100

    chore: update project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * Proteins
+ *
+ * (c) 2017 Chialab (http://www.chialab.it) <dev@chialab.io>
+ * http://chialab.io
+ *
+ * A primer for JavaScript libraries and frameworks development.
+ */
+
+import clone from './clone.js';
+import merge from './merge.js';
+import equivalent from './equivalent.js';
+import mix from './mixin.js';
+import Symbolic from './symbolic.js';
+import Observable from './observable.js';
+import * as keypath from './keypath.js';
+import * as Factory from './factory.js';
+import * as Proto from './proto.js';
+import * as Url from './url.js';
+
+export * from './types.js';
+export * from './events.js';
+export { Symbolic };
+export { Observable };
+export { clone };
+export { merge };
+export { equivalent };
+export { mix };
+export { keypath };
+export { Url };
+export { Factory };
+export { Proto };
