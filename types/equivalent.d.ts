6f202f3a297de971e6e54d8fd4f01c3058516e2d
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-05-23 16:37:30 +0200

    chore: update project dependencies

--- types/equivalent.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Recursive objects equivalence check.
- * @param {*} obj1 The original object.
- * @param {*} obj2 The object to compare
- * @return {Boolean}
- */
-export default function equivalent(obj1: any, obj2: any): boolean;
--- types/has.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Exec Object.prototype.hasOwnProperty against an object.
- * @param {Object} scope The scope object to check.
- * @param {string} property The property name to check.
- * @return {boolean}
- */
-export default function has(scope: Object, property: string): boolean;
